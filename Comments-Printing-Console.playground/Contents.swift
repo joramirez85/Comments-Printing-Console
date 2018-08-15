6885a47dc70b8f1703fe214a0e251cf97fbe13d7
Jesus Ramirez <jesus.ramirez@GDLA-MC-180723B.local>
2018-08-14 22:36:11 -0500

    initial commit

---
+++ Comments-Printing-Console.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+
+var monsterHealth = 19
+
+monsterHealth = monsterHealth + 22
+
+monsterHealth = monsterHealth / 2
+
+print(monsterHealth)
+
+print("Hello World")
+
+// Example of a comment in one line
+
+/*
+ Example
+ of
+ a comment
+ in many lines
+ */
